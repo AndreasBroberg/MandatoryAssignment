657d19fa6c4a2da8d1880594f3d0188f7575dd43
AndreasBroberg <90028853+AndreasBroberg@users.noreply.github.com>
2023-04-27 22:40:36 +0200

    27/04/23

--- MandatoryAssignment/Program.cs
+++ MandatoryAssignment/Program.cs
@@ -8,8 +8,6 @@ while (world.Hero.IsAlive && world.Creatures.Count != 0)
     ConsoleKey c = Console.ReadKey().Key;
     world.MoveHero(world.CharToNumber(c));
     world.PrintWorld();
-    Console.WriteLine(world.Creatures.Count);
-    world.PositionsList.ForEach(pos => Console.WriteLine(pos));
 }
 string victoryString = world.Hero.IsAlive ? "You won!" : "You lost!";
 Log.GetLogger().CloseLogger();
